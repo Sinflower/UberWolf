fcbe91fd7c90fddddc9bde8af9949dde5d5b6e0d
Sinflower <s1nflower@yandex.com>
2024-04-06 18:50:30 +0200

    UberWolfCli  - Added help text  - Properly implemented support of the basic features (extract based on game.exe, data folder or list of wolf files)

--- UberWolfCli/UberWolfCli.cpp
+++ UberWolfCli/UberWolfCli.cpp
@@ -24,24 +24,88 @@
  *
  */
 
+#include <filesystem>
 #include <iostream>
-#include <windows.h>
 #include <vector>
+#include <windows.h>
 
 #include <UberWolfLib.h>
+#include <Utils.h>
+
+namespace fs = std::filesystem;
+
+static const std::string UWCLI_VERSION = "0.1.0";
+static const std::string UWCLI_NAME  = "UberWolfCli";
+
+void printHelp()
+{
+	std::cout << "UberWolfCli v" << UWCLI_VERSION << std::endl;
+	std::cout << "Usage: " << std::endl;
+	std::cout << "  " << UWCLI_NAME << " <Game[Pro].exe>" << std::endl;
+	std::cout << "  " << UWCLI_NAME << " <data_folder>" << std::endl;
+	std::cout << "  " << UWCLI_NAME << " <.wolf-files>" << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
-	UberWolfLib uwl(argc, argv);
+	if (IsSubProcess())
+	{
+		UberWolfLib uwl;
+		return 0;
+	}
 
-	UWLExitCode uec = uwl.UnpackData();
+	tStrings args          = argvToList(argc, argv);
+	const tStrings zeroArg = { args[0] };
+	UberWolfLib uwl(zeroArg);
 
-	std::string key;
+	// If no arguments are passed, print the help message
+	if (args.size() == 1)
+	{
+		printHelp();
+		return 0;
+	}
 
-	if(uwl.FindProtectionKey(key) == UWLExitCode::SUCCESS)
-		std::cout << "Protection key: " << key << std::endl;
+	// Check if the first argument is an executable
+	if (fs::exists(args[1]) && fs::is_regular_file(args[1]) && fs::path(args[1]).extension() == ".exe")
+	{
+		uwl.InitGame(args[1]);
+		uwl.UnpackData();
+
+		std::string key;
+
+		if (uwl.FindProtectionKey(key) == UWLExitCode::SUCCESS)
+			std::cout << "Protection key: " << key << std::endl;
+
+		return 0;
+	}
+
+	tStrings paths;
+
+	// Check if the first argument is a folder
+	if (fs::exists(args[1]) && fs::is_directory(args[1]))
+	{
+		for (const auto& entry : fs::directory_iterator(args[1]))
+		{
+			if (entry.is_regular_file())
+				paths.push_back({ FS_PATH_TO_TSTRING(entry.path()) });
+		}
+	}
 	else
-		std::cout << "Protection key not found!" << std::endl;
+	{
+		for (size_t i = 1; i < args.size(); i++)
+		{
+			if (fs::exists(args[i]) && fs::is_regular_file(args[i]))
+				paths.push_back({ args[i] });
+		}
+	}
+
+	if (paths.empty())
+	{
+		std::cout << "No valid files found." << std::endl;
+		return -1;
+	}
+
+	uwl.UnpackDataVec(paths);
 
 	return 0;
 }
